b23d8405529b151c3488a873b58b4cdb61facb61
Merlin Unterfinger <info@munterfinger.ch>
2020-11-27 22:19:44 +0100

    [pkg] Format install and check script.

--- check.sh
+++ check.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
-# ----------------------------------------------------------------------------- 
+# -----------------------------------------------------------------------------
 # Name          :checks.sh
 # Description   :Script for running all package (pytest, coverage, mypy and
 #                flake8) checks.
 # Author        :Merlin Unterfinger <info@munterfinger.ch>
 # Date          :2020-10-14
-# Version       :0.1.0  
+# Version       :0.1.0
 # Usage         :./checks.sh
-# Notes         :       
+# Notes         :
 # Bash          :5.0.18
 # =============================================================================
 
@@ -18,7 +18,7 @@ echo -e '\n*** mypy: Static type checks ***'
 poetry run mypy --config-file pyproject.toml .
 
 echo -e '\n*** flake8: Code linting ***'
-poetry run flake8 . --count --exit-zero --max-complexity=10 --statistics 
+poetry run flake8 . --count --exit-zero --max-complexity=10 --statistics
 
 echo -e '\n*** Building documentation ***'
 cd docs && poetry run make html && cd ..
