6b46eb0a19f770e2c36f1a4492857218791012aa
Merlin Unterfinger <info@munterfinger.ch>
2022-03-01 16:55:17 +0100

    Remove init file.

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
-
 """Tests for `glacier_flow_model.data` module."""
+from osgeo.gdal import Dataset
 
 from glacier_flow_model.data import PkgDataAccess
-from osgeo.gdal import Dataset
 
 pkg = PkgDataAccess(verbose=True)
 
